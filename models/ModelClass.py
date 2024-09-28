75084109be4195a82aa013a58c28c7fa868fffd4
Jakub Janicki <janicus321@gmail.com>
2024-09-28 22:40:54 +0200

    change return type of predict method to np.array

--- models/ModelClass.py
+++ models/ModelClass.py
@@ -1,12 +1,18 @@
-import torch
 import torch.nn as nn
+import numpy as np
 
 class ModelClass:
     def __init__(self, model) -> None:
         self._model = model
 
-    def predict(self, x) -> torch.Tensor:
-        return self._model.predict(x)
+    def predict(self, x) -> np.ndarray:
+        y = self._model.predict(x)
+        if not isinstance(y, np.ndarray):
+            try:
+                y = np.array(y)
+            except:
+                raise ValueError(f"Model output is not a numpy array and cannot be converted to one: {y}")
+        return y
 
     @property
     def model(self) -> nn.Module:
