fdbdf2016115dec44ae46e23b0b0cfbb3a0928bb
Jakub Janicki <jakubjanicki360@gmail.com>
2024-11-12 14:42:53 +0100

    update setup and requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 import subprocess
 import sys
+import os
 
-torch_commands = [
-    'pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124',
-    'pip3 install -r requirements.txt'
-]
+
 # assert python 3.11.X
 if sys.version_info < (3, 11):
     print('Please use Python 3.11.X')
     sys.exit(1)
 
-for command in torch_commands:
+requirements = [
+    'pip3 install -r requirements.txt',
+    'pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124'
+]
+for command in requirements:
     subprocess.run(command, shell=True)
