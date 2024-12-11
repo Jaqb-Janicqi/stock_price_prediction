5d2fd12a3b8d73eafab016e8278bd4beb7ca24d5
Jakub Janicki <jakubjanicki360@gmail.com>
2024-12-11 14:18:32 +0100

    change linux execution

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ def linux_install():
         "pip install ta-lib"
     ]
     for command in commands:
-        subprocess.check_call(command.split(), shell=True)
+        subprocess.run(command, shell=True)
 
 
 def install_talib():
