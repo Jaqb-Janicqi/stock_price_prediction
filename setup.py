24b170f79a79d7626a5306de24c71905f29d3f64
Jakub Janicki <jakubjanicki360@gmail.com>
2024-12-11 13:24:53 +0100

    add on-the-fly installation of requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,51 @@
 import subprocess
 import sys
 import os
+import zipfile
 
 
-# assert python 3.11.X
-if sys.version_info < (3, 11):
-    print('Please use Python 3.11.X')
-    sys.exit(1)
+def install_talib():
+    try:
+        import talib
+        return
+    except ImportError:
+        pass
 
-requirements = [
-    'pip3 install -r requirements.txt',
-    'pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124'
-]
-for command in requirements:
-    subprocess.run(command, shell=True)
+    import requests
+    directory = 'talib'
+    os.makedirs(directory, exist_ok=True)
+    url = "https://github.com/TA-Lib/ta-lib-python/files/12437040/TA_Lib-0.4.28-cp311-cp311-win_amd64.whl.zip"
+    f_path = os.path.join(directory, "TA_Lib-0.4.28-cp311-cp311-win_amd64.whl.zip")
+    with open(f_path, "wb") as f:
+        f.write(requests.get(url).content)
+    with zipfile.ZipFile(f_path, "r") as zip_ref:
+        zip_ref.extractall(directory)
+    subprocess.check_call(["pip", "install", os.path.join(directory, "TA_Lib-0.4.28-cp311-cp311-win_amd64.whl")])
+
+    # check if talib is installed properly
+    try:
+        import talib
+    except ImportError:
+        print('Automatic TA-Lib installation failed. Please install TA-Lib manually.')
+        sys.exit(1)
+
+
+def install_requirements():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+
+
+def run_setup():
+    # assert python 3.11.X
+    if sys.version_info < (3, 11):
+        print('Please use Python 3.11.X')
+        sys.exit(1)
+
+    # install requirements
+    install_requirements()
+
+    # check if talib is installed and install if not
+    install_talib()
+    
+
+if __name__ == '__main__':
+    run_setup()
