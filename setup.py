9ec01e03ee9f82db02878303937f7ef1ed2d8af4
Jakub Janicki <jakubjanicki360@gmail.com>
2024-12-11 13:42:37 +0100

    handle installing talib on unix

--- setup.py
+++ setup.py
@@ -11,17 +11,25 @@ def install_talib():
     except ImportError:
         pass
 
-    import requests
-    directory = 'talib'
-    os.makedirs(directory, exist_ok=True)
-    url = "https://github.com/TA-Lib/ta-lib-python/files/12437040/TA_Lib-0.4.28-cp311-cp311-win_amd64.whl.zip"
-    f_path = os.path.join(directory, "TA_Lib-0.4.28-cp311-cp311-win_amd64.whl.zip")
-    with open(f_path, "wb") as f:
-        f.write(requests.get(url).content)
-    with zipfile.ZipFile(f_path, "r") as zip_ref:
-        zip_ref.extractall(directory)
-    subprocess.check_call(["pip", "install", os.path.join(directory, "TA_Lib-0.4.28-cp311-cp311-win_amd64.whl")])
-
+    os_version = sys.platform
+    # check if linux or unix
+    if os_version.startswith('linux'):
+        import requests
+        subprocess.check_call(["pip", "debug", "--verbose"])
+        directory = 'talib'
+        os.makedirs(directory, exist_ok=True)
+        url = "https://github.com/TA-Lib/ta-lib-python/files/12437040/TA_Lib-0.4.28-cp311-cp311-win_amd64.whl.zip"
+        f_path = os.path.join(directory, "TA_Lib-0.4.28-cp311-cp311-win_amd64.whl.zip")
+        with open(f_path, "wb") as f:
+            f.write(requests.get(url).content)
+        with zipfile.ZipFile(f_path, "r") as zip_ref:
+            zip_ref.extractall(directory)
+        subprocess.check_call(["pip", "install", os.path.join(directory, "TA_Lib-0.4.28-cp311-cp311-win_amd64.whl")])
+    else:
+        # https://www.wheelodex.org/projects/talib-binary/wheels/talib_binary-0.4.19-cp37-cp37m-manylinux1_x86_64.whl/
+        url = 'https://files.pythonhosted.org/packages/00/61/a68a9276a3c166df8717927780d994496ee4cb5299903a409f93689a2b4e/talib_binary-0.4.19-cp37-cp37m-manylinux1_x86_64.whl'
+        subprocess.check_call(["pip", "install", url])
+    
     # check if talib is installed properly
     try:
         import talib
