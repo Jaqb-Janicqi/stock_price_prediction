4129c2a5599034a5e36851de24caffdad7c3a1e0
Jakub Janicki <jakubjanicki360@gmail.com>
2024-12-11 17:14:37 +0100

    deploy using conda for talib and install requirements with pip on target machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from app import start_app_from_terminal
-
-
 if __name__ == '__main__':
-    start_app_from_terminal()
+    from setup import install_requirements
+    install_requirements()
+    from app import main
+    main()
